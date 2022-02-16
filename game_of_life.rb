8c46169dc66eff1a11c604b57a0271fe1f462b4a
EliasUR <eliasrodriguez2002@hotmail.com>
2022-02-16 20:58:06 -0300

    add neighbours counting
    neighbours couting added and refactor the grid from * and . to 1 and 0
    
    Co-authored-by: alanRazoMen <arazo@ucol.mx>
    Co-authored-by: nallely64 <nallelygar.64@gmail.com>
    Co-authored-by: juperameza <juperameza@hotmail.com>

--- game_of_life.rb
+++ game_of_life.rb
@@ -5,19 +5,76 @@ puts 'Insert rows:'
 x = gets.chomp.to_i
 puts 'Inserte columns:'
 y = gets.chomp.to_i
+m = Matrix.build(x,y){ rand(2) }
 
-m = Matrix.build(x, y) do
-  if rand(2) == 1
-    '.'
-  else
-    '*'
+def show_m (x, y, m)
+x.times do |i|
+  y.times do |j|
+    if m[i, j] == 1
+      print '*'
+    else
+      print '.'
+    end
   end
+  puts ''
+ end
 end
 
 puts 'generation 1'
+show_m(x, y, m)
+
 x.times do |i|
-  y.times do |j|
-    print m[i, j]
+  y.times do |j|   
+    neighbors = 0
+    unless (i==0 || j==0)
+     neighbors += m[i-1,j-1]
+    end
+    unless i==0
+      neighbors += m[i-1,j] 
+    end
+    unless j==y-1 || i==0
+    neighbors += m[i-1,j+1] 
+    end
+    unless j==0
+     neighbors += m[i,j-1] 
+    end
+    unless j==y-1
+    neighbors += m[i,j+1] 
+    end
+    unless i==x-1 || j==0
+    neighbors += m[i+1,j-1] 
+    end
+    unless i==x-1
+    neighbors += m[i+1,j] 
+    end
+    unless i==x-1 || j==y-1
+    neighbors += m[i+1,j+1] 
+    end
+    print neighbors
   end
-  puts ''
+  puts ""
 end
+
+
+# m[i-1, j-1] 
+# m[i-1, j] 
+# m[i-1, j+1]
+# m[i, j-1]    
+# m[ij]
+# m[ij+1]
+# m[i+1j-1]    
+# m[i+1j]  
+# m[i+1j+1]
+
+# if(i==0 && j==0)
+#   neighbors += m[i+1,j]
+#   neighbors += m[i+1,j+1]
+#   neighbors += m[i,j+1]
+# elsif(i==0)
+#   neighbors = 0
+#   neighbors += m[i+1,j]
+#   neighbors += m[i-1,j]
+#   neighbors += m[i-1,j+1]
+#   neighbors += m[i,j+1]
+#   neighbors += m[i+1,j+1]
+# end
